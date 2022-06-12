c34a615a42a37753abf1e0b0b3ad1f77d8d5cc33
[ritual] ../*.shen <106227931+Yppf00@users.noreply.github.com>
2022-06-12 21:06:13 +0200

    Add files via upload

---
+++ dark-light mode/main.js
@@ -0,0 +1,12 @@
+let toggler = document.querySelector('#toggler');
+let body = document.querySelector('body');
+let h2 = document.querySelector('h2');
+let h3 = document.querySelector('h3');
+let p = document.querySelector('p');
+
+toggler.addEventListener('click', function () {
+   body.classList.toggle('dark-background');
+   h2.classList.toggle('light');
+   h3.classList.toggle('light');
+   p.classList.toggle('light');
+});
